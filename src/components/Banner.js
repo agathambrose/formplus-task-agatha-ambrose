79695668e4b2a642887e13511790c2f9c5b0c586
Agatha Ambrose <73320658+agathambrose@users.noreply.github.com>
2021-05-26 22:47:56 +0100

    file restructuring

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ export const Banner = () => {
     <div className="mt-10">
       <div className="flex justify-center items-center bg-red-100 p-3 mx-auto">
         <BsInfoCircle className="text-yellow-500 font-bold text-7xl md:text-2xl mr-8" />
-        <h4 className="text-sm md:text-sm font-medium">
+        <h4 className="text-sm md:text-sm font-medium tracking-wide text-justify">
           Tada! Get started with a free template. Can’t find what you are
           looking for? Search from the 1000+ available templates
         </h4>
