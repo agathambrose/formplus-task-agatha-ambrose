69baadd84531d50109c037efd2bff1a23190b1fb
Agatha Ambrose <73320658+agathambrose@users.noreply.github.com>
2021-06-05 01:38:13 +0100

    Pagination ui done

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,24 +5,26 @@ export const Pagination = () => {
   const [pages, setPages] = useState(1);
 
   return (
-    <div className="flex text-neautraltext justify-between items-center font-circular mb-3 px-4">
+    <div className="flex items-center justify-between px-4 mb-3 text-neautraltext font-circular">
       <button
         className={`font-medium text-base border-none ${
           pages === 1 ? "cursor-not-allowed" : ""
         } bg-white outline-none focus:outline-none`}
+        onClick={() => setPages(pages - 1)}
       >
         Previous
       </button>
       <div className="flex items-center space-x-2">
-        <button className="font-medium text-base px-3 py-1 bg-white rounded border">
+        <button className="px-3 py-1 text-base font-medium bg-white border rounded">
           {pages}
         </button>
-        <h3 className="font-normal text-base">of 200</h3>
+        <h3 className="text-base font-normal">of 200</h3>
       </div>
       <button
         className={`font-medium text-base flex items-center border-none ${
           pages === 1 ? "cursor-pointer" : ""
         } bg-white outline-none focus:outline-none`}
+        onClick={() => setPages(pages + 1)}
       >
         Next
         <GrFormNext className="ml-2" />
